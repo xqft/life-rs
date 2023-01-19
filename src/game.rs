654068ab7b0395d5f0f9bbe5720386e0f9ecd9e1
xqft <estefano.bargas@fing.edu.uy>
2023-01-19 14:04:19 -0300

    Created Position struct for row and col of a cell

--- src/game.rs
+++ src/game.rs
@@ -6,6 +6,11 @@ pub enum Cell {
     Alive
 }
 
+pub struct Position {
+    row: usize,
+    col: usize
+}
+
 pub struct Grid {
     pub width: usize,
     pub height: usize,
@@ -26,7 +31,7 @@ impl Grid {
 
         for (i, row) in self.cells.iter().enumerate() {
             for (j, cell) in row.iter().enumerate() {
-                new.cells[i][j] = match (cell, self.live_neighbours(i, j)) {
+                new.cells[i][j] = match (cell, self.live_neighbours(Position {row: i, col: j})) {
                     (Cell::Alive, 0 | 1)    => Cell::Dead,
                     (Cell::Alive, 4..)      => Cell::Dead,
                     (Cell::Dead, 3)         => Cell::Alive,
@@ -38,13 +43,13 @@ impl Grid {
         new
     }
 
-    fn get_neighbours(&self, row: usize, col: usize) -> Vec<Cell> {
+    fn get_neighbours(&self, pos: Position) -> Vec<Cell> {
         let mut result = Vec::new();
 
-        let imin = row.checked_sub(1).unwrap_or(0);
-        let imax = cmp::min(self.height - 1, row + 1);
-        let jmin = col.checked_sub(1).unwrap_or(0);
-        let jmax = cmp::min(self.width - 1, col + 1);
+        let imin = pos.row.checked_sub(1).unwrap_or(0);
+        let imax = cmp::min(self.height - 1, pos.row + 1);
+        let jmin = pos.col.checked_sub(1).unwrap_or(0);
+        let jmax = cmp::min(self.width - 1, pos.col + 1);
 
         for i in imin..=imax {
             for j in jmin..=jmax {
@@ -55,11 +60,11 @@ impl Grid {
         result
     }
 
-    pub fn live_neighbours(&self, row: usize, col: usize) -> usize {
-        self.get_neighbours(row, col)
+    pub fn live_neighbours(&self, pos: Position) -> usize {
+        self.get_neighbours(pos)
             .iter()
             .filter(|cell| **cell == Cell::Alive)
             .count()
-            - if self.cells[row][col] == Cell::Alive { 1 } else { 0 } // so it doesn't count itself.
+            - if self.cells[pos.row][pos.col] == Cell::Alive { 1 } else { 0 } // so it doesn't count itself.
     }
 }
